6d9dd80b6bf40781e75e1d43f82b858409b83a62
Dima Anosov <anosov.work@gmail.com>
2021-10-08 12:12:29 +0300

    Updates

---
+++ precache-manifest.9cfd254f5cdde3e22d1dfbe92c04e545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d7c07102852d78a265c1d78fb100c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c03b8446ac256301c978",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03b8446ac256301c978",
+    "url": "./static/js/main.e1ad0385.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
